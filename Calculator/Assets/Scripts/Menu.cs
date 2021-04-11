f0c67275a40f82471e539e5dc23ef877ec948753
PuppyLo <g-rasul@mail.ru>
2021-04-11 21:29:43 +0300

    First

---
+++ Calculator/Assets/Scripts/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public  bool PauseMenu;
+    public GameObject PauseMenuUI;
+
+    public void Start()
+    {
+        Time.timeScale = 1f;
+        PauseMenu = false;
+    }
+
+    public void MenuButton()
+    {
+        if (PauseMenu)
+
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+   private void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PauseMenu = false;
+    }
+
+    private void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu = true;
+    }
+
+    public void SelectMenu()
+    {
+        SceneManager.LoadScene("Scenes/SelectScene");
+    }
+    
+}
